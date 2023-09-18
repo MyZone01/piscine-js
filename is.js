6a041f5231171b646481f82a57476ee87d0624fa
serignmbaye <barsaliou1998@gmail.com>
2023-09-18 13:31:06 +0000

    fix: is

--- is.js
+++ is.js
@@ -7,6 +7,7 @@ is.undef = (u) => typeof u === 'undefined'
 is.def = (d) => !typeof u === 'undefined'
 is.arr = (a) => Array.isArray(a)
 is.obj = (o) => (typeof o === 'object' || o === null) && !Array.isArray(o)
+is.obj = (o) => typeof(o) === 'object' ? toString.call(o).slice(8, -1) === 'Array' ? "" : o === null ? "" : typeof o:  "" ;
 is.fun = (f) => typeof f === 'function'
 is.truthy = (t) => t == true
 is.falsy = (f) => f == false
