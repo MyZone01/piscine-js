906e9c8be31f6a4257dc317eff4a50f0c2c45f51
serignmbaye <barsaliou1998@gmail.com>
2023-09-18 12:33:01 +0000

    fix: mutability

--- mutability.js
+++ mutability.js
@@ -1,17 +1,6 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-}
-
 const clone1 = Object.assign({}, person)
 const clone2 = Object.assign({}, person)
 const samePerson = person
 
 person.age++
 person.country = 'FR'
-
-console.log("person:", person);
-console.log("clone1:", clone1);
-console.log("clone2:", clone2);
-console.log("samePerson:", samePerson);
