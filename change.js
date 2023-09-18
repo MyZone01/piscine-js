5657cf074676be336385e8184085d0723ea8f03a
serignmbaye <barsaliou1998@gmail.com>
2023-09-18 12:51:09 +0000

    fix: change

--- change.js
+++ change.js
@@ -1,8 +1,2 @@
-const sourceObject = {
-    num: 42,
-    bool: true,
-    str: 'some text',
-    log: console.log,
-}
 const get = (key) => sourceObject[key]
 const set = (key, value) => sourceObject[key] = value
