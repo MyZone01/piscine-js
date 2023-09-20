4820241e833f3291dd60d71093435ac7134e9b7b
serignmbaye <barsaliou1998@gmail.com>
2023-09-20 18:12:33 +0000

    fix: flat

--- flat.js
+++ flat.js
@@ -8,10 +8,3 @@ const flat = (arr, depth = 1) => {
     return newArr;
 }
 
-const arr1 = [0, 1, 2, [3, 4]];
-
-console.log(flat(arr1));
-
-const arr2 = [0, 1, [2, [3, [4, 5]]]];
-
-console.log(flat(arr2, 4));
