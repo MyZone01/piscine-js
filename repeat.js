52e9e1c614b7800741431487efb8e8f696ea12ef
serignmbaye <barsaliou1998@gmail.com>
2023-09-20 13:07:25 +0000

    fix: repeat

--- repeat.js
+++ repeat.js
@@ -1,2 +1,1 @@
-const repeat = (strs, number) => [...Array(number+1)].reduce((acc, v) => acc = acc ? acc + strs : strs)
-
+const repeat = (strs, number) => number ? [...Array(number+1)].reduce((acc, v) => acc = acc ? acc + strs : strs) : ""
