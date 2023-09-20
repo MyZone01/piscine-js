80e3ef98e295cc924212d2df3660570964ba7e4c
serignmbaye <barsaliou1998@gmail.com>
2023-09-20 16:44:54 +0000

    feat: nasa

--- repeat.js
+++ repeat.js
@@ -1,1 +1,2 @@
-const repeat = (strs, number) => number ? [...Array(number+1)].reduce((acc, v) => acc = acc ? acc + strs : strs) : ""
+const repeat = (strs, number) => number ? [...Array(number+1)]
+.reduce((acc, v) => acc = acc ? acc + strs : strs) : ""
