ea1bbbbcde94c343934dcf630d8a189e5a50e13e
serignmbaye <barsaliou1998@gmail.com>
2023-09-23 18:29:54 +0000

    fix: greedy url

--- greedy-url.js
+++ greedy-url.js
@@ -1,13 +1,22 @@
-const getURL = (str) => str.match(/https?:\/\/\S+/g)
-const greedyQuery = (str) => str.match(/https?:\/\/\S+&\S+&\S+/g)
-const _notSoGreedy = (str) => str.match(/https?:\/\/\S+\?(\S+=\S+&){1,2}\S+=\S+/g)
+const getURL = (dataSet) => {
+    let regex = /(((https?:\/\/)|(www\.))[^\s]+)/g
+    return dataSet.match(regex)
+}
+
+const greedyQuery = (dataSet) => {  
+    let regex = /(https?:\/\/[^\s]+\?[^\s]+=[^\s]+&[^\s]+=[^\s]+&[^\s]+=[^\s]+)/g;    
+    return dataSet.match(regex); 
+}
+
 const notSoGreedy = (dataSet) => {
     let res = getURL(dataSet)
     let regex = /=/g
     let result = []
-    res.forEach(el => {
-        match = el.match(regex)
-        match !== null && match.length >= 2 && match.length <= 3 && result.push(res[i])
-    });
+    for (let i=0; i < res.length; i++){
+        let match = res[i].match(regex)
+        if (match !== null && match.length >= 2 && match.length <= 3){
+            result.push(res[i])
+        }
+    }   
     return result;  
 }
