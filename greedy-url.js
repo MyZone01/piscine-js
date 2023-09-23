7cf03be439fd4168d94c271c34bdcaef58731ffe
serignmbaye <barsaliou1998@gmail.com>
2023-09-23 16:44:04 +0000

    fix: greedy url

--- greedy-url.js
+++ greedy-url.js
@@ -1,3 +1,13 @@
 const getURL = (str) => str.match(/https?:\/\/\S+/g)
 const greedyQuery = (str) => str.match(/https?:\/\/\S+&\S+&\S+/g)
-const notSoGreedy = (str) => str.match(/https?:\/\/\S+\?(\S+=\S+&){1,2}\S+=\S+/g)
+const _notSoGreedy = (str) => str.match(/https?:\/\/\S+\?(\S+=\S+&){1,2}\S+=\S+/g)
+const notSoGreedy = (dataSet) => {
+    let res = getURL(dataSet)
+    let regex = /=/g
+    let result = []
+    res.forEach(el => {
+        match = el.match(regex)
+        match !== null && match.length >= 2 && match.length <= 3 && result.push(res[i])
+    });
+    return result;  
+}
