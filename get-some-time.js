bd6617e79313ff07d53235d008e0812cb10311b4
serignmbaye <barsaliou1998@gmail.com>
2023-09-25 16:25:09 +0000

    fix: get some time

--- get-some-time.js
+++ get-some-time.js
@@ -11,7 +11,7 @@ const firstDayWeek = (week, year) => {
     const JanuaryFirst = new Date(year, 0, 1);
     if (week === 1) return formatDate(JanuaryFirst.getDate(), JanuaryFirst.getMonth() + 1, JanuaryFirst.getFullYear());
     const daysToAdd = (week - 1) * 7;
-    const firstDay = new Date(JanuaryFirst.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
+    const firstDay = new Date(JanuaryFirst.setHours(24 * daysToAdd));
     let weekDay = firstDay.getDay();
     weekDay = weekDay === 0 ? 6 : weekDay - 1;
     if (weekDay !== 1) firstDay.setHours(-24 * weekDay)
