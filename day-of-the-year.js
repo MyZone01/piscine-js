487780f51e1a725f8cad06aba68a8d805472c912
serignmbaye <barsaliou1998@gmail.com>
2023-09-25 17:50:48 +0000

    fix: day of the year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -7,10 +7,3 @@ const dayOfTheYear = (date) => {
     const days = Math.round(timeDiff / (1000 * 60 * 60 * 24)) + 1;
     return days;
 }
-
-console.log(dayOfTheYear(new Date('0001-01-01')), 1)
-console.log(dayOfTheYear(new Date('1664-08-09')), 222)
-console.log(dayOfTheYear(new Date('1600-12-31')), 366)
-console.log(dayOfTheYear(new Date('2020-06-22')), 174)
-console.log(dayOfTheYear(new Date('2048-12-08')), 343)
-console.log(dayOfTheYear(new Date('2048-11-08')), 313)
