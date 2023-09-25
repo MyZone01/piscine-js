5cf5f2c5acdd21708dd24c1deb6a86dee3e47c03
serignmbaye <barsaliou1998@gmail.com>
2023-09-25 16:14:42 +0000

    feat: get some time

--- get-some-time.js
+++ get-some-time.js
@@ -1,23 +1,22 @@
+const formatDate = (day, month, _year) => `${day.toString().padStart(2, '0')}-${month.toString().padStart(2, '0')}-${_year}`;
+
 /**
  * Calculates the date of the first day of a given week in a specific year.
- *
- * @param {number} week - The week number.
- * @param {number} year - The year.
- * @returns {string} The date of the first day of the week in the format dd-mm-yyyy.
- */
-function firstDayWeek(week, year) {
+*
+* @param {number} week - The week number.
+* @param {number} year - The year.
+* @returns {string} The date of the first day of the week in the format dd-mm-yyyy.
+*/
+const firstDayWeek = (week, year) => {
     const JanuaryFirst = new Date(year, 0, 1);
+    if (week === 1) return formatDate(JanuaryFirst.getDate(), JanuaryFirst.getMonth() + 1, JanuaryFirst.getFullYear());
     const daysToAdd = (week - 1) * 7;
     const firstDay = new Date(JanuaryFirst.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
+    let weekDay = firstDay.getDay();
+    weekDay = weekDay === 0 ? 6 : weekDay - 1;
+    if (weekDay !== 1) firstDay.setHours(-24 * weekDay)
     const day = firstDay.getDate();
     const month = firstDay.getMonth() + 1;
-    const formattedDate = `${day.toString().padStart(2, '0')}-${month.toString().padStart(2, '0')}-${year}`;
-    return formattedDate;
+    const _year = firstDay.getFullYear();
+    return formatDate(day, month, _year);
 }
-
-console.log(firstDayWeek(1, '1000'), '01-01-1000')
-console.log(firstDayWeek(52, '1000'), '22-12-1000')
-console.log(firstDayWeek(2, '0001'), '08-01-0001')
-console.log(firstDayWeek(43, '1983'), '17-10-1983')
-console.log(firstDayWeek(23, '0091'), '04-06-0091')
-console.log(firstDayWeek(2, '2017'), '02-01-2017')
