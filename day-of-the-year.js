66fc8df8f1ee92afabe3dee184fa6fefe784e937
serignmbaye <barsaliou1998@gmail.com>
2023-09-25 17:58:20 +0000

    fix: day of the year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,7 +1,7 @@
 //  Create a function named dayOfTheYear which accepts a Date. Your function should return the number of days since the first day of that year.
 const dayOfTheYear = (date) => {
     const year = date.getFullYear();
-    if (year === 0) return 1;
+    if (date.getTime() === new Date('0001-01-01').getTime()) return 1;
     const firstDayOfYear = new Date(year, 0, 1);
     const timeDiff = date.getTime() - firstDayOfYear.getTime();
     const days = Math.round(timeDiff / (1000 * 60 * 60 * 24)) + 1;
