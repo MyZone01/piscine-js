8b9d479b43de70474a2a4e2ee2cf5a2a92060307
serignmbaye <barsaliou1998@gmail.com>
2023-09-26 16:54:33 +0000

    feat: mapper

---
+++ mapper.js
@@ -0,0 +1,15 @@
+// Create a map function that takes an array as the first argument, a function as second, and that works like the method .map
+const map = (arr, func) => {
+    const mappedArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        mappedArr.push(func(arr[i]));
+    }
+    return mappedArr;
+}
+
+// Create a flatMap function that takes an array as the first argument, a function as second, and that works like the method .flatMap
+const flatMap = (arr, func) => {
+    return arr.reduce((acc, val) => {
+        return acc.concat(func(val));
+    }, []);
+}
