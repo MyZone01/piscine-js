59c4cd06e43240a0164026ccd2c81359e4251932
serignmbaye <barsaliou1998@gmail.com>
2023-09-26 17:13:03 +0000

    feat: filter

---
+++ filter.js
@@ -0,0 +1,25 @@
+// Create the following functions, which each take an array as the first argument, and a function as the second argument.
+// filter: that works like the [].filter method.
+const filter = (arr, func) => {
+    const filteredArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            filteredArray.push(arr[i]);
+        }
+    }
+    return filteredArray;
+}
+
+// reject: that works like the reject function from lodash.
+const reject = (arr, func) => {
+    const rejected = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!func(arr[i])) {
+            rejected.push(arr[i]);
+        }
+    }
+    return rejected;
+}
+
+// partition: that works like the partition function from lodash.
+const partition = (arr, func) => [filter(arr, func), reject(arr, func)];
