ffd7277af5611a831367aa889bcee47292ba3831
serignmbaye <barsaliou1998@gmail.com>
2023-09-26 17:02:19 +0000

    fix: mapper

--- mapper.js
+++ mapper.js
@@ -2,14 +2,14 @@
 const map = (arr, func) => {
     const mappedArr = [];
     for (let i = 0; i < arr.length; i++) {
-        mappedArr.push(func(arr[i] || 0));
+        mappedArr.push(func(arr[i], i, arr));
     }
     return mappedArr;
 }
 
 // Create a flatMap function that takes an array as the first argument, a function as second, and that works like the method .flatMap
 const flatMap = (arr, func) => {
-    return arr.reduce((acc, val) => {
-        return acc.concat(func(val));
+    return arr.reduce((acc, val, i) => {
+        return acc.concat(func(val, i, arr));
     }, []);
 }
