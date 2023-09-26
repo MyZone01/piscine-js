82a275e3484e7f3b1354ef4b522901fb752230ef
papa gondia gueye <papgueye@noreply.learn.zone01dakar.sn>
2023-09-26 20:52:24 +0000

    je

--- currify.js
+++ currify.js
@@ -1,6 +1,2 @@
 // Create the function currify that will curry any functions put as argument.
-const currify = (func) => {
-    const curried = (...args) => (args.length >= func.length) ? func(...args) : (...moreArgs) => curried(...args, ...moreArgs)
-    return curried;
-};
-
+const currify = (func) => curried = (...args) => (args.length >= func.length) ? func(...args) : (...moreArgs) => curried(...args, ...moreArgs)
