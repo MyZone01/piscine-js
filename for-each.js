68eadb4942cf9a4537850bc431c5a44bf1d69188
serignmbaye <barsaliou1998@gmail.com>
2023-09-26 16:50:24 +0000

    fix: for each

--- for-each.js
+++ for-each.js
@@ -1,2 +1,7 @@
 // Create a function named forEach which takes an array as the first argument, a function as the second argument, and that works like the Array.prototype.forEach method.
-const forEach = (arr, func,) => { arr.map((element, index, arr) => func(element, index, arr)); }
+const forEach = (arr, func,) => {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        func(element, i, arr);
+    }
+}
