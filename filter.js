48a99bf3e7917e83fae2a1bf5c485e72434842f1
serignmbaye <barsaliou1998@gmail.com>
2023-09-26 17:15:24 +0000

    fix: filter

--- filter.js
+++ filter.js
@@ -3,7 +3,7 @@
 const filter = (arr, func) => {
     const filteredArray = [];
     for (let i = 0; i < arr.length; i++) {
-        if (func(arr[i])) {
+        if (func(arr[i], i, arr)) {
             filteredArray.push(arr[i]);
         }
     }
@@ -14,7 +14,7 @@ const filter = (arr, func) => {
 const reject = (arr, func) => {
     const rejected = [];
     for (let i = 0; i < arr.length; i++) {
-        if (!func(arr[i])) {
+        if (!func(arr[i], i, arr)) {
             rejected.push(arr[i]);
         }
     }
@@ -23,3 +23,15 @@ const reject = (arr, func) => {
 
 // partition: that works like the partition function from lodash.
 const partition = (arr, func) => [filter(arr, func), reject(arr, func)];
+
+
+const check1 = (el) => el % 2 === 0
+const check2 = (el, i) => el % 3 === 0 && i % 2 === 0
+const check3 = (el) => Array.isArray(el)
+const check4 = (el, i, arr) =>
+    typeof el !== 'number' && i % 2 !== 0 && arr.includes(true)
+const check5 = (el, i) =>
+    (typeof el === 'number' || typeof el === 'boolean') && i > 5
+const check6 = (el) => el.region === 'South' || el.region === 'West'
+
+console.log(filter(ctx.onlyNumbers, check2), [15, 33, 450, 66])
