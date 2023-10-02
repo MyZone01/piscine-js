0cf7e23b958725602db24e60a2f27452c4fd2c1a
serignmbaye <barsaliou1998@gmail.com>
2023-10-02 10:12:58 +0000

    feat: deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,8 +1,18 @@
+/**
+ * Creates a deep copy of an object or an array.
+ *
+ * @param {Object|Array} obj - The object or array to be deep copied.
+ * @return {Object|Array} - The deep copied object or array.
+ */
 const deepCopy = (obj) => {
+    const keys = Object.keys(obj)
     if (Array.isArray(obj))
         return obj.map((item) => deepCopy(item));
     else if (typeof obj === 'object' && obj !== null)
-        return Object.keys(obj).reduce((acc, key) => { if (obj.hasOwnProperty(key)) acc[key] = deepCopy(obj[key]); return acc; }, {});
+        if (keys.length === 0)
+            return obj
+        else
+            return keys.reduce((acc, key) => { if (obj.hasOwnProperty(key)) acc[key] = deepCopy(obj[key]); return acc; }, {});
     else
         return obj;
 };
