264e2293f92a9d2ca03d4e2a136afde924ba380f
serignmbaye <barsaliou1998@gmail.com>
2023-10-02 08:41:08 +0000

    feat: manipulate keys

---
+++ manipulate-keys.js
@@ -0,0 +1,32 @@
+// Function similar to filter for object keys
+const filterKeys = (obj, callback) => {
+    const filteredObj = {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (callback(key)) {
+                filteredObj[key] = obj[key];
+            }
+        }
+    }
+    return filteredObj;
+}
+
+// Function similar to map for object keys
+const mapKeys = (obj, callback) => {
+    const mappedObj = {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            mappedObj[callback(key)] = obj[key];
+        }
+    }
+    return mappedObj;
+}
+
+// Function similar to reduce for object keys
+const reduceKeys = (obj, callback, initialValue) => {
+    let accumulator = initialValue === undefined ? keys[0] : initialValue;
+    for (const key of Object.keys(obj)) {
+        accumulator = callback(accumulator, key);
+    }
+    return accumulator;
+}
