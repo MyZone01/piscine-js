50c13f3e1ef62824457bf5e76a0bbbc14f3b0cd3
serignmbaye <barsaliou1998@gmail.com>
2023-10-06 22:42:37 +0000

    fix: gougle search

--- gougle-search.js
+++ gougle-search.js
@@ -5,7 +5,9 @@
  * @param {string} q - The query to send to the server.
  * @return {Promise} A promise that resolves to the JSON response from the server.
  */
-const queryServers = (serverName, q) => Promise.race([getJSON(`/${serverName}?q=${q}`), getJSON(`/${serverName}_backup?q=${q}`)]);
+const queryServers = (serverName, q) => Promise.any([getJSON(`/${serverName}?q=${q}`), getJSON(`/${serverName}_backup?q=${q}`)]).then((result) => result).catch ((e) => {
+    throw e
+});
 
 /**
  * Performs a search on multiple servers for the given query.
@@ -13,23 +15,24 @@ const queryServers = (serverName, q) => Promise.race([getJSON(`/${serverName}?q=
  * @param {string} q - The search query.
  * @return {Object} An object containing the search results for web, image, and video.
  */
-const gougleSearch = async (q) => {
-    const webPromise = queryServers('web', q);
-    const imagePromise = queryServers('image', q);
-    const videoPromise = queryServers('video', q);
-
-    const results = Promise.race([
-        await Promise.all([webPromise, imagePromise, videoPromise]),
-        new Promise((_, reject) => {
-            setTimeout(() => {
-                reject(new Error('timeout'));
-            }, 80);
-        })
+async function gougleSearch(query) {
+    let result = await Promise.all([ // return the fastest response from each server
+        await timeout(80, queryServers('web', query)),
+        await timeout(80, queryServers('image', query)),
+        await timeout(80, queryServers('video', query))
     ])
+    return { 'web': result[0], 'image': result[1], 'video': result[2] }
+}
 
-    return {
-        web: results[0],
-        image: results[1],
-        video: results[2],
-    };
-};
+/**
+ * Executes a callback function with a timeout.
+ *
+ * @param {number} delay - The delay in milliseconds before the timeout.
+ * @param {function} callback - The callback function to execute.
+ * @return {Promise} A promise that resolves with the result of the callback function or rejects with an error if the timeout occurs.
+ */
+function timeout(delay, callback) {
+    return Promise.race([callback, new Promise((_, reject) => { // return the fastest response
+        setTimeout(() => reject(new Error("timeout")), delay)
+    })])
+}
