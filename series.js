3149e8f2ac62df60793c3f3805b8d0274d1332d2
serignmbaye <barsaliou1998@gmail.com>
2023-10-06 23:16:55 +0000

    fix: series

--- series.js
+++ series.js
@@ -1,7 +1,13 @@
 /**
  * Runs an array of asynchronous functions in series and returns an array of their results.
  *
- * @param {Array<Function>} asyncFuncs - An array of asynchronous functions to be executed in series.
+ * @param {Array<Function>} funcs - An array of asynchronous functions to be executed in series.
  * @return {Promise<Array>} - A promise that resolves to an array of the results of each function.
 */
-const series = async (asyncFuncs) => await asyncFuncs.map(async func => await func());
+// const series = async (funcs) => await funcs.map(async func => await func());
+
+async function series(funcs) {
+    let result = []
+    for (let i = 0; i < funcs.length; i++) result.push(await funcs[i]())
+    return result
+}
