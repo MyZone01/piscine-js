6f5922bcee3cd504dda17a884ba435cd36ff424a
serignmbaye <barsaliou1998@gmail.com>
2023-10-06 22:47:00 +0000

    fix: series

--- series.js
+++ series.js
@@ -4,4 +4,4 @@
  * @param {Array<Function>} asyncFuncs - An array of asynchronous functions to be executed in series.
  * @return {Promise<Array>} - A promise that resolves to an array of the results of each function.
 */
-const series = async (asyncFuncs) => asyncFuncs.reduce(async (acc, func) => acc.push(await func()), []);
+const series = async (asyncFuncs) => asyncFuncs.map(async func => await func());
